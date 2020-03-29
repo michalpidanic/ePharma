71bdd57eb28690fcc38cfd81eaecb6a7946e6c10
Michal <michal.pidanic1@gmail.com>
2020-03-29 13:53:43 +0200

    edit in pharmacy package

--- ePharma/src/Pharmacy/Medicine.java
+++ ePharma/src/Pharmacy/Medicine.java
@@ -1,4 +1,8 @@
 package Pharmacy;
 
 public class Medicine {
+    protected String name;
+    protected String application;
+    public double price;
+    protected boolean prescription;
 }
--- ePharma/src/Pharmacy/Order.java
+++ ePharma/src/Pharmacy/Order.java
@@ -1,4 +1,13 @@
 package Pharmacy;
 
 public class Order {
+    protected int orderId;
+    protected int itemsAdded;
+    protected double totalPrice;
+
+    public Order () {
+        this.orderId = ;
+        this.itemsAdded = ;
+        this.totalPrice = ;
+    }
 }
