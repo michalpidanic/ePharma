eb393900c8b7a64053b1002f0ba45e642b66d9e7
Michal <michal.pidanic1@gmail.com>
2020-05-06 12:26:22 +0200

    modified item view and controller, modified css files for sidebar in home view and for button in item view

--- ePharma/src/GUI/Controllers/HomeController.java
+++ ePharma/src/GUI/Controllers/HomeController.java
@@ -11,7 +11,7 @@ import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class HomeController implements Initializable {
     @FXML
     private Button btnMenu;
 
